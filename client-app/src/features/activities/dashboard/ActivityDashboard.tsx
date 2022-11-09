b4a418731ead61ca7e1aae67cd7679c65ed1d052
eraldalikcani <eralda.likcani@fshnstudent.info>
2022-11-09 15:46:27 +0100

    End of CRUDs in React

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid, List } from 'semantic-ui-react';
+import { Grid} from 'semantic-ui-react';
 import { Activity } from '../../../app/models/activity';
 import ActivityDetails from '../details/ActivityDetails';
 import ActivityForm from '../form/ActivityForm';
@@ -13,14 +13,19 @@ interface Props {
     editMode: boolean;
     openForm: (id: string) => void;
     closeForm: () => void;
-
+    createOrEdit: (activity: Activity) => void;
+    deleteActivity: (id: string) => void;
 }
 
-export default function ActivityDashboard({activities, selectActivity,selectedActivity,cancelSelectActivity, editMode, openForm, closeForm} : Props) {
+export default function ActivityDashboard({activities, selectActivity,selectedActivity,cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity} : Props) {
     return(
         <Grid>
             <Grid.Column width='10'>
-                <ActivityList activities={activities} selectActivity={selectActivity}/>
+                <ActivityList 
+                activities={activities} 
+                selectActivity={selectActivity}
+                deleteActivity={deleteActivity}
+                />
             </Grid.Column>
             <Grid.Column width='6'>
                 {selectedActivity && !editMode &&
@@ -30,7 +35,7 @@ export default function ActivityDashboard({activities, selectActivity,selectedAc
                     openForm={openForm}
                 />}
                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity}></ActivityForm>}
+                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}></ActivityForm>}
             </Grid.Column>
         </Grid>
     )
