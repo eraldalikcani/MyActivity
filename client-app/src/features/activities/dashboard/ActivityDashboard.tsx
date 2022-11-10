34c548d0db717ef813af8fa5bcca7043f452cc71
eraldalikcani <eralda.likcani@fshnstudent.info>
2022-11-10 14:42:19 +0100

    data from react to api fixed

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,9 +15,12 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
-export default function ActivityDashboard({activities, selectActivity,selectedActivity,cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity} : Props) {
+export default function ActivityDashboard({activities, selectActivity,selectedActivity,
+    cancelSelectActivity, editMode, openForm, closeForm, createOrEdit, deleteActivity,
+    submitting} : Props) {
     return(
         <Grid>
             <Grid.Column width='10'>
@@ -25,6 +28,7 @@ export default function ActivityDashboard({activities, selectActivity,selectedAc
                 activities={activities} 
                 selectActivity={selectActivity}
                 deleteActivity={deleteActivity}
+                submitting={submitting}
                 />
             </Grid.Column>
             <Grid.Column width='6'>
@@ -35,7 +39,11 @@ export default function ActivityDashboard({activities, selectActivity,selectedAc
                     openForm={openForm}
                 />}
                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}></ActivityForm>}
+                <ActivityForm 
+                closeForm={closeForm} 
+                activity={selectedActivity} 
+                createOrEdit={createOrEdit}
+                submitting={submitting}></ActivityForm>}
             </Grid.Column>
         </Grid>
     )
